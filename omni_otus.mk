67c66c4e9f8076e2fb15533b4d308ee4b5dfff3a
sultanqasim <sultanqasim@gmail.com>
2015-04-09 20:24:31 -0400

    Fix missed detail

--- omni_otus.mk
+++ omni_otus.mk
@@ -17,6 +17,6 @@
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-$(call inherit-product, device/motorola/surnia/full_otus.mk)
+$(call inherit-product, device/motorola/otus/full_otus.mk)
 
 PRODUCT_NAME := omni_otus
