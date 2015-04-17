3327f0329a990527d4d15c55926240b346a1fd9d
Sultan Qasim Khan <sqkhan@motorola.com>
2015-04-16 21:06:33 -0400

    Added some board config options

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,8 +14,6 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/motorola/otus
-
 TARGET_ARCH := arm
 TARGET_CPU_ABI  := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
@@ -32,7 +30,6 @@ TARGET_BOARD_PLATFORM := msm8610
 TARGET_BOOTLOADER_BOARD_NAME := MSM8610
 TARGET_NO_BOOTLOADER := true
 
-
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 utags.blkdev=/dev/block/platform/msm_sdcc.1/by-name/utags movablecore=160M vmalloc=400M
 BOARD_KERNEL_BASE := 0x0
 BOARD_RAMDISK_OFFSET := 0x01000000
@@ -43,13 +40,19 @@ TARGET_KERNEL_SOURCE := kernel/motorola/msm8610
 TARGET_KERNEL_CONFIG := msm8610-mini_defconfig
 
 BOARD_USES_QCOM_HARDWARE := true
-
+GET_USE_QCOM_BIONIC_OPTIMIZATION := true
+TARGET_USES_QCOM_BSP := true
+COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
 
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
 TARGET_RECOVERY_FSTAB := device/motorola/otus/twrp.fstab
 
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00A00000
+BOARD_FLASH_BLOCK_SIZE := 0x20000
+BOARD_HAS_LARGE_FILESYSTEM := true
 
 # TWRP
 DEVICE_RESOLUTION := 540x960
@@ -58,5 +61,6 @@ RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TW_INCLUDE_CRYPTO := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_IGNORE_MAJOR_AXIS_0 := true
-
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+BOARD_SUPPRESS_EMMC_WIPE := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
