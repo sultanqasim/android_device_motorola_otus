90467a3b5f997eea5f19c21be6805077756a5ec0
Sultan Qasim Khan <sqkhan@motorola.com>
2015-04-20 16:05:15 -0400

    Revert "Use appended dtb kernel"
    This reverts commit 7d6b4e9532bce6eace468ffec99dd720cccdaa91.

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -2,17 +2,14 @@ LOCAL_PATH := $(call my-dir)
 
 ## Imported from the original makefile...
 KERNEL_CONFIG := $(KERNEL_OUT)/.config
-KERNEL_ZIMG = $(KERNEL_OUT)/arch/arm/boot/zImage-dtb
+KERNEL_ZIMG = $(KERNEL_OUT)/arch/arm/boot/zImage
+DTC = $(KERNEL_OUT)/scripts/dtc/dtc
+
 
 ## Build and run dtbtool
 DTBTOOL := $(TARGET_KERNEL_SOURCE)/tools/dtbtool/dtbtool
 $(DTBTOOL):
 	make -C $(TARGET_KERNEL_SOURCE)/tools/dtbtool
-
-# Hack to build appended DTB zImage and make the build system think it is a normal zImage
-$(KERNEL_ZIMG): $(INSTALLED_KERNEL_TARGET)
-	make -C $(TARGET_KERNEL_SOURCE) zImage-dtb O=$(KERNEL_OUT) ARCH=$(TARGET_ARCH) $(ARM_CROSS_COMPILE)
-	cp $(KERNEL_ZIMG) $(INSTALLED_KERNEL_TARGET)
 
 INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
 
@@ -22,14 +19,15 @@ $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/u
 	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm/boot/
 	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
 
+
 ## Overload bootimg generation: Same as the original, + --dt arg
-$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET) $(KERNEL_ZIMG)
+$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET)
 	$(call pretty,"Target boot image: $@")
 	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
 
 ## Overload recoveryimg generation: Same as the original, + --dt arg
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) $(recovery_ramdisk) $(recovery_kernel) $(KERNEL_ZIMG)
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) $(recovery_ramdisk) $(recovery_kernel)
 	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
